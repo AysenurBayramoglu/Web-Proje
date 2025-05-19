d9bd3fb58410416807e56311412b233b297a9688
Ayşenur <aysenur.bayramoglu23@ogr.atauni.edu.tr>
2025-05-19 23:28:18 +0300

    kullanici_onayla.php

--- kullanici_onayla.php
+++ kullanici_onayla.php
@@ -1,12 +1,11 @@
 <?php
 include("baglanti.php");
-
+// onaylaya basılırsa kullanıcının id'sine göre onaylı kısmı 1 yapılır.
 if (isset($_POST['onayla'])) {
-    //kullanıcı id sini alır.
     $id = intval($_POST['id']);
-    //o id ye ait kullanıcıyı onaylar.
     mysqli_query($baglanti, "UPDATE kullanicilar SET onayli=1 WHERE id=$id");
-} elseif (isset($_POST['sil'])) {
+}
+elseif (isset($_POST['sil'])) {//sile basılırsa kullanıcı veri tabanından tamamen silinir.
     $id = intval($_POST['id']);
     mysqli_query($baglanti, "DELETE FROM kullanicilar WHERE id=$id");
 }
